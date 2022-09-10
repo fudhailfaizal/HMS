ab9e381d656eec25dda0fafb9f65cbe553f64453
fudhailfaizal <92314938+fudhailfaizal@users.noreply.github.com>
2022-09-10 20:10:06 +0530

    Add files via upload

---
+++ hospital website/login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medcare - Hospital Management System </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <section class="book" id="book">
+   
+        <h1 class="heading"> <span>Login</span>  </h1>   
+         
+    
+        <div class="row">
+    
+            <div class="image">
+                <img src="image/book-img.svg" alt="">
+            </div>
+    
+            <form action="">
+                <h3>Login</h3>
+                <input type="text" required placeholder="Email / Username" class="box">
+                <input type="password" required placeholder="password" class="box">
+                <p> Dont have an account? Register now! </p>
+                
+                <input type="submit" value="Login" class="btn">
+                
+                <a href="register.php" link text class="btn"> Register </a>
+            </form>
+    
+        </div>
+    
+    </section>
+    
---
+++ hospital website/register.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Medcare - Hospital Management System </title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    <section class="book" id="book">
+    
+
+        <h1 class="heading"> <span>Register</span>  </h1>  
+        <a href="login.php" link text class="btn"> Back to Login </a>  
+    
+        <div class="row">
+    
+            <div class="image">
+                <img src="image/book-img.svg" alt="">
+            </div>
+    
+            <form action="">
+                <h3>Register</h3>
+                <input type="email" required placeholder="Email / Username" class="box">
+                <input type="text" required placeholder="Name" class="box">
+                <input type="text" required placeholder="Age" class="box">
+                <input type="password" required placeholder="password" class="box">
+                
+                <input type="submit" value="Register" class="btn">
+            </form>
+    
+        </div>
+    
+    </section>
+    
